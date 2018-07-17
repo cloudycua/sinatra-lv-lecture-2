e01e8928ecae402b78f21dd8dd681c3c42b8f841
Christine Cua <cloudycua@gmail.com>
2018-07-17 14:48:37 +0000

    controller notes

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,25 @@
 class PostsController < ApplicationController
+# BUILDING THESE ACTIONS IN THIS CONTROLLER.
+# EVERY CONTROLLER SHOULD BE ABOUT ONE RESOURCE
+# IT SHOULD IMPLEMENT AROUND 7 ACTIONS AND NO MORE
 
+#  resources #=> What our application is exposing to the world view
+
+#  "photos"
+
+#  GET /photos #=> index, list all the photos
+#  GET /photos/1 #=> show, show you a particular photo based on input
+
+#  GET /photos/new #=> new, should present a form for new photos
+#  POST /photos #=> create, should add a new photo to my application
+
+#  GET /photos/1/edit #=> edit, should present a form to edit a photo
+#  PUT /photos/1 #=> update, should update photo 1
+
+#  DELETE /photos/1 #=> destroy, should delete photo 1
+
+
+  # Using homepage to list all Blog Posts (breaking convention)
   get '/' do # posts#index action
     @posts = Post.all
 
@@ -24,5 +44,22 @@ class PostsController < ApplicationController
 
     erb :"posts/show.html" # render the posts/show template
   end
+
+
+
+  # posts#new action
+
+
+  # posts#create action
+
+
+  # posts@edit
+
+
+  # posts#update
+
+
+
+  # posts#destroy
 
 end
